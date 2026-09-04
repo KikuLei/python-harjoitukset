2b89970cbe32ec136269e7938b90166ced62216f
Kia Leino <kialei@metropolia.fi>
2026-09-04 15:38:06 +0300

    Lisätty uudet harjoitustehtävät

--- Peliprojekti/main.py
+++ Peliprojekti/main.py
@@ -3,3 +3,25 @@ ikä = int(input('Anna pelaajanikä: '))
 
 print('Pelaajan nimi:', nimi)
 print('Pelaajan ikä:', ikä)
+
+if ikä < 12:
+    print('Olet liian nuori käyttämään peliä.')
+else:
+    print("Tervetuloa", nimi + "!")
+    komento = ""
+    while komento != 'lopeta':
+        print()
+        print('PÄÄVALIKKO')
+        print('lennä')
+        print('tutki')
+        print('tankkaa')
+        print('lopeta')
+        komento = input('Anna komento: ')
+        if komento == 'lennä':
+            print('Avaruusalus lähtee kohti uutta planeettaa!')
+        elif komento == 'tutki':
+            print('Tutkit tuntematonta planeettaa ja löydät hylätyn avaruusaluksen')
+        elif komento == 'tankkaa':
+            print('Avaruusalus tankattu!')
+        elif komento == 'lopeta':
+            print('Avaruusseikkailu päättyy!')
---
+++ Tuntitehtävät/tuntitehtävä3.py
@@ -0,0 +1,68 @@
+a = 5
+b = 7
+a *= b
+print(a)
+
+#kerta = 1
+#while kerta < 5:
+    #print('hello')
+
+#kerta = 1
+#while kerta <= 5:
+    #print('hello')
+    #kerta += 1
+
+#luku = 1
+#while luku <= 20:
+    #print(luku)
+    #luku += 1
+
+#if luku % 2 == 0:
+    #print('parillinen')
+    #print('pariton')
+
+#while luku <= 20:
+    #print(luku)
+    #luku += 2
+
+#luku = int (input('Anna luku: \n'))
+#while luku >= 0:
+   # print(luku)
+  #  luku -= 1
+ #   if luku == 0:
+  #      print('kaboom!')
+#oikea1 = 'James'
+#oikea2 = 'Bond'
+#salasana = input('Anna salasana: ')
+#while salasana != oikea1:
+    #print('väärä salasana, kokeile uudelleen!')
+   # salasana =input('Anna salasana')
+  #  if salasana == oikea2:
+#        break
+  #  else:
+  #      print('Nyt else kohta')
+
+import random
+#luku = random.randint('1,2')
+#if luku == 1:
+   # print('Kruunu tuli')
+#elif luku == 2:
+  #  print('Klaava tuli')
+  
+noppa1 = noppa2 =heitot = 0
+while (noppa1 != 3 or noppa2 != 3):
+    noppa1 = random.randint(1,3)
+    noppa2 = random.randint(1.3)
+    noppa3 = random.randint(1,3)
+    heitot + 1
+    
+
+
+
+
+
+
+
+
+
+
